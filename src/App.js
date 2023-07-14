1f511454911a4f60108b0d5b4fce772705a9c87a
Rbihag <bihag.rudolf@gmail.com>
2023-07-14 21:34:18 +0800

    Added Basic pages and layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
 import './App.css';
+
+import React from 'react';
+
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from 'react-router-dom';
+
+import Layout from './components/Layout';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <h1>Hello from app!</h1>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
