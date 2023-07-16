67dd1cccf72a7901313b9ffd3e7bb6c5e6d4538f
Rbihag <bihag.rudolf@gmail.com>
2023-07-16 21:09:12 +0800

    complete services section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -54,7 +54,51 @@ const Home = () => {
                     </div>
                 </div>
             </section>
-
+            <section className='home-wrapper-2 py-5'>
+                <div className='container-xxl'>
+                    <div className='row'>
+                        <div className='col-12'>
+                            <div className='services d-flex align-items-center justify-content-between'>
+                                <div className='d-flex align-items-center gap-15'>
+                                    <img src='images/service.png' alt='services' />
+                                    <div>
+                                        <h6>Free Shipping</h6>
+                                        <p className="mb-0">For Orders Over &#8369;500</p>
+                                    </div>
+                                </div>
+                                <div className='d-flex align-items-center gap-15'>
+                                    <img src='images/service-02.png' alt='services' />
+                                    <div>
+                                        <h6>Daily Surprises</h6>
+                                        <p className="mb-0">Save Up To 30% off</p>
+                                    </div>
+                                </div>
+                                <div className='d-flex align-items-center gap-15'>
+                                    <img src='images/service-03.png' alt='services' />
+                                    <div>
+                                        <h6>Support 24/7</h6>
+                                        <p className="mb-0">After Sales Support</p>
+                                    </div>
+                                </div>
+                                <div className='d-flex align-items-center gap-15'>
+                                    <img src='images/service-04.png' alt='services' />
+                                    <div>
+                                        <h6>Affordable Prices</h6>
+                                        <p className="mb-0">Guaranteed Low Price</p>
+                                    </div>
+                                </div>
+                                <div className='d-flex align-items-center gap-15'>
+                                    <img src='images/service-05.png' alt='services' />
+                                    <div>
+                                        <h6>Secure Payments</h6>
+                                        <p className="mb-0">100% Protected Payments</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     );
 };
