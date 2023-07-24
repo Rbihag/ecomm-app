a5d203835ac7055bbfd22a65c2e6936f0e0eda00
Rbihag <bihag.rudolf@gmail.com>
2023-07-24 16:44:22 +0800

    start checkout page

---
+++ src/pages/Checkout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Meta from '../components/Meta'
+import BreadCrumb from '../components/BreadCrumb'
+
+const Checkout = () => {
+    return (
+        <>
+            <Meta title={'Checkout'} />
+            <BreadCrumb title='Checkout' />
+        </>
+    )
+}
+
+export default Checkout
